cab5ae4f2ccd3b4f08ab3b1de5e94753c0d4f322
NikitaBorate <104753385+NikitaBorate@users.noreply.github.com>
2023-05-25 06:00:43 +0530

    Add files via upload

---
+++ 1Bscript.js
@@ -0,0 +1,23 @@
+const submit = () => {
+    var Name = document.getElementById("First Name").value;
+    var LastName = document.getElementById("Last Name").value;
+    var MotherName = document.getElementById("Mother Name").value;
+    var FatherName = document.getElementById("Father Name").value;
+    var Address = document.getElementById("Address").value;
+    var DOB = document.getElementById("DOB").value;
+    var Pincode = document.getElementById("Pincode").value;
+    var Course = document.getElementById("Course").value;
+    var EmailID = document.getElementById("Email ID").value;
+
+    console.log(Name);
+    console.log(LastName);
+    console.log(MotherName);
+    console.log(FatherName);
+    console.log(Address);
+    console.log(DOB);
+    console.log(Pincode);
+    console.log(Course);
+    console.log(EmailID);
+}
+
+    
